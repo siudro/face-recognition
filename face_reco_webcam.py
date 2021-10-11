997c028fe0beb35ec267f8f484e01b646b862496
siudro <83130573+siudro@users.noreply.github.com>
2021-10-11 22:35:30 +0300

    Add files via upload

---
+++ face_reco_webcam.py
@@ -0,0 +1,36 @@
+import numpy as np
+import face_recognition as fr
+import cv2
+
+video_capture= cv2.VideoCapture(0)
+the_image= fr.load_image_file("sedra.jpg")
+the_face_encoding =fr.face_encoding(the_image)[0]
+
+known_face_encodings= [the_face_encoding]
+known_face_names = ["سدره"]
+
+while True:
+    ret, frame = video_capture.read()
+    rgb_frame = frame[:, :, ::-1]
+
+    face_locations= fr.face_locations(rgb_frame)
+    face_encodings = fr.face_encoding(rgb_frame, face_locations)
+
+    for(top, right, bottom, left),face_encoding in zip(face_locations, face_encodings):
+        matches = fr.compare_faces(known_face_encodings, face_encoding)
+        name= 'غير معروف'
+        face_distances = fr.face_distance(known_face_encodings, face_encoding)
+        best_match_index= np.argmin(face_distances)
+        if matches[best_match_index]:
+            name=known_face_names[best_match_index]
+
+        cv2.rectangle(frame, (left, top),(right, bottom),(0,0,255),3)
+        cv2.rectangle(frame,(left, bottom -35), (right, bottom),(0,0,255),cv2.filled)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, name, (left +6, bottom -6), font, 1.0,(255,255,255), 1)
+        cv2.imshow('webcam_facerecognition', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        video_capture.release()
+        cv2.destroyAllWindows()
